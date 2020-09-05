227fd31da610939a231fb4a43061f47ab4cc75b7
Samarth <samarthasthana110@gmail.com>
2020-09-05 21:01:48 +0530

    l

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,11 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 // /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render("index");
-=======
-// GET home page. 
-router.get('/', function (req, res) {
-    res.send('start project');
->>>>>>> 11758fdfcc38967bd7148e8d00a57ba5b8525323
 })
-
 // get registration page
 router.get('/register', function (req, res) {
     res.render('register');
