3e435ae8936a994fd0abda48e6ca3ef75901993f
RahilRehan <rahilmuti12345@gmail.com>
2020-09-06 04:22:36 +0530

    donations and events

---
+++ models/donation-model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const donationSchema = new Schema({
+    createdUser: String,
+    title: String,
+    body: String,
+})
+
+const Donation = mongoose.model("donation", donationSchema);
+
+module.exports = Donation;
