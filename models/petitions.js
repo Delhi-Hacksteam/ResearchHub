0ef4c626ed49a1e03a674bbf74d4b20c6fb048a4
ritika-0111 <51761546+ritika-0111@users.noreply.github.com>
2020-09-06 04:25:24 +0530

    creating petitons

---
+++ models/petitions.js
@@ -0,0 +1,23 @@
+let mongoose = require('mongoose');
+
+//petitions schema
+let dataSchema = mongoose.Schema({
+  title:{
+    type: String,
+    required: true
+  },
+  content:{
+    type: String,
+    required: true
+  },
+  email:{
+    type: String,
+    required: true
+  },
+  name:{
+    type: String,
+    required: true
+  }, timestamp:{ type: Date, default: Date.now }
+});
+
+let Data = module.exports = mongoose.model('Data', dataSchema);
--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,62 @@ var authCheck = require("./auth-check")
 var bodyParser = require('body-parser');
 const User = require('../models/user-model');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+let Data = require('../models/petitions');
+var upload = require('../utils/upload');
 
 // GET home page
 router.get('/', function (req, res, next) {
-    res.render("index");
+    res.render("index",  { user: req.user });
 })
-// get registration page
-router.get('/register', function (req, res) {
-    res.render('register');
+// get petiton page
+router.get('/petition', function (req, res) {
+    res.render('petition', { user: req.user });
 })
+// upload page 
+router.get('/upload', ensureAuthenticated, function (req, res) {
+  res.render('upload', { user: req.session });
+});
 
+// upload post  
+router.post('/upload', upload.single('myfile'), (req, res) => {
+  let data = new Data({
+    title: req.body.title,
+    content: req.body.content,
+    email:  req.body.email,
+    name: req.file.filename
+  });
+  data.save(function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log('data', req.body);
+      console.log('data added');
+      res.redirect('/posts/1');
+    }
+  });
+  console.log(req.body);
+  console.log('Name ', req.file.filename);
+});
+
+// Access Control
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/login');
+  }
+}
+
+/*
 // Landing page
-router.get('/', authCheck, (req, res) => {
+router.get('/detail', authCheck, (req, res) => {
     res.redirect("/index",);
     // res.send("you are logged in as " + req.user.username);
-})
+}) */
 
-router.get('/index', (req, res) => {
-    res.render("index", { user: req.user });
+router.post('/contact', (req, res) => {
+    res.render("contact", { user: req.user });
 }
 )
 
